91cbe99f7ac8f936460dee4860f93ce4b93fa95a
Huzaifa355 <huzaiashafiq@gmail.com>
2025-05-31 13:19:00 +0500

    Retrain the model and remove bugs and improve debuging to monitor every single step

--- espcam/espcam.ino
+++ espcam/espcam.ino
@@ -8,7 +8,6 @@
 #define XCLK_GPIO_NUM      0
 #define SIOD_GPIO_NUM     26
 #define SIOC_GPIO_NUM     27
-
 #define Y9_GPIO_NUM       35
 #define Y8_GPIO_NUM       34
 #define Y7_GPIO_NUM       39
@@ -17,7 +16,6 @@
 #define Y4_GPIO_NUM       19
 #define Y3_GPIO_NUM       18
 #define Y2_GPIO_NUM        5
-
 #define VSYNC_GPIO_NUM    25
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
@@ -33,20 +31,25 @@ void handleRoot() {
 }
 
 void handleCapture() {
-  // Turn on flash just before capture and off immediately after
-  digitalWrite(LED_GPIO_NUM, HIGH);
-  
+  digitalWrite(LED_GPIO_NUM, HIGH); // Turn on flash
+  delay(100); // Brief delay to stabilize lighting
+
   camera_fb_t* fb = esp_camera_fb_get();
-  digitalWrite(LED_GPIO_NUM, LOW);
-  
+  digitalWrite(LED_GPIO_NUM, LOW); // Turn off flash
+
   if (!fb) {
     server.send(500, "text/plain", "Camera capture failed");
     return;
   }
 
+  Serial.printf("Captured image: %d bytes\n", fb->len);
+
+  server.setContentLength(fb->len);
   server.sendHeader("Content-Type", "image/jpeg");
   server.sendHeader("Connection", "close");
-  server.send_P(200, "image/jpeg", (const char*)fb->buf, fb->len);
+  server.send(200, "image/jpeg", "");  // Send headers only
+  server.client().write(fb->buf, fb->len);  // Send JPEG image data
+
   esp_camera_fb_return(fb);
 }
 
@@ -54,7 +57,6 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
 
-  // Setup LED pin
   pinMode(LED_GPIO_NUM, OUTPUT);
   digitalWrite(LED_GPIO_NUM, LOW);
 
@@ -81,13 +83,12 @@ void setup() {
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
-  // Adjust these settings for better performance
   if (psramFound()) {
-    config.frame_size = FRAMESIZE_SVGA; // 800x600 - better than 96x96
-    config.jpeg_quality = 12;           // Lower quality = faster
+    config.frame_size = FRAMESIZE_SVGA; // 800x600
+    config.jpeg_quality = 12;           // Lower = faster
     config.fb_count = 2;
   } else {
-    config.frame_size = FRAMESIZE_VGA;   // 640x480
+    config.frame_size = FRAMESIZE_VGA;  // 640x480
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
@@ -99,15 +100,16 @@ void setup() {
     delay(1000);
     ESP.restart();
   }
+
   Serial.println("Camera initialized");
 
-  // WiFi setup with timeout
+  // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   Serial.print("Connecting to WiFi");
-  unsigned long startTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) {
+  unsigned long startAttempt = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttempt < 10000) {
     delay(500);
     Serial.print(".");
   }
@@ -122,6 +124,7 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  // HTTP routes
   server.on("/", HTTP_GET, handleRoot);
   server.on("/capture", HTTP_GET, handleCapture);
   server.begin();
